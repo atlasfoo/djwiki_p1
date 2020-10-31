70f921ac8826440deeebd75b3aa344ea31fbe32f
atlasfoo@lin <jf_mejiar547@live.com>
2020-10-31 00:16:58 -0600

    :construction::art: - base project specifications

--- dj_wiki/settings.py
+++ dj_wiki/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dj_wiki.apps.encyclopedia',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
