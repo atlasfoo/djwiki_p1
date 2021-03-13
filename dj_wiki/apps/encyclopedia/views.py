6861f6477a949ab10873abc5a1f52221c7b81822
atlasfoo@lin <jf_mejiar547@live.com>
2021-03-13 17:45:25 -0600

    search implemented

--- dj_wiki/apps/encyclopedia/views.py
+++ dj_wiki/apps/encyclopedia/views.py
@@ -1,5 +1,5 @@
 import markdown2
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from . import util
 
@@ -22,3 +22,22 @@ def wiki_entry(request, title):
     entry_text = markdown2.markdown(entry)
 
     return render(request, "encyclopedia/wiki_entry.html", {"body": entry_text})
+
+
+def search(request):
+    """Searches for an entry in current ones, if name fully matches,
+    directly redirect, if not, redirects to a results page"""
+    query = str(request.GET['q'])
+
+    entry = util.get_entry(title=query)
+
+    # if exact matches, redirect to directly
+    if entry:
+        return redirect('wiki_entry', title=query)
+
+    # look for full list
+    all_entries = util.list_entries()
+    # filter titles starting with
+    filtered_entries = [entry for entry in all_entries if entry.lower().startswith(query.lower())]
+
+    return render(request, "encyclopedia/results.html", {"result": filtered_entries})
