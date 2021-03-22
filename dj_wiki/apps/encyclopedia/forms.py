8f94071aed9f4c8e3f3b4ba74ba4739c31bff13f
atlasfoo@lin <jf_mejiar547@live.com>
2021-03-22 00:19:21 -0600

    entry body textarea help text updated

--- dj_wiki/apps/encyclopedia/forms.py
+++ dj_wiki/apps/encyclopedia/forms.py
@@ -6,5 +6,6 @@ class EntryForm(forms.Form):
                             required=True, widget=forms.TextInput(attrs={"class": "form-control"}),
                             error_messages={'required': 'Please write a entry title'})
     entry = forms.CharField(label='Entry body', widget=forms.Textarea(attrs={"class": "form-control"}), required=True,
-                            error_messages={'required': 'Please write a entry body'})
-
+                            error_messages={'required': 'Please write a entry body'},
+                            help_text='You can use <a href="https://www.markdownguide.org/basic-syntax/" '
+                                      'target="_blank">Markdown</a> to format the entry text')
